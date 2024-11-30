2c6b4648d2a74a28db64f92359109c24aebbf065
desmondlch <desmondleongch@gmail.com>
2024-12-01 00:35:05 +0800

    pass

---
+++ Models/UserModel.cs
@@ -0,0 +1,13 @@
+﻿namespace angular_spa.Models
+{
+    public class UserModel
+    {
+        public string Name { get; set; }
+
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+
+        public string RefreshToken { get; set; }
+    }
+}
